c9d89f869977039ed752fa2b812135a73b6aa8ad
jasper-bober <112430968+jasper-bober@users.noreply.github.com>
2026-02-11 00:07:42 -0500

    Add files via upload

---
+++ Week4/Bober_Lab4-1.ps1
@@ -0,0 +1,37 @@
+﻿
+
+function ApacheLogs1(){
+
+    $logsNotFormatted = Get-Content C:\xampp\apache\logs\access.log | Select-String "404"
+
+    $LogTable = @()
+    for($i=0; $i -lt $logsNotFormatted.Count; $i++){
+
+
+    $words = ([String]$logsNotFormatted[$i]).split(" ");
+
+    $LogTable += [pscustomobject]@{ "IP" = $words[0] 
+                                    "Time" = $words[3] 
+                                    "Method" = $words[5]
+                                    "Page" = $words[6]
+                                  "Protocol" = $words[7]
+                                  "Response" = $words[8]
+                                  "Referrer" = $words[10]
+    
+    }
+
+
+}
+
+# Write-Host ($logTable | Group-Object IP | Format-Table -AutoSize | Out-String)
+
+return $LogTable
+
+
+}
+
+$CallingFunction = ApacheLogs1
+Write-Host ($logTable | Group-Object IP | Format-Table -AutoSize | Out-String)
+
+$CallingFunction
+
---
+++ Week4/Bober_Lab4-2.ps1
@@ -0,0 +1,8 @@
+﻿.(Join-Path $PSScriptRoot Bober_LogLab-1.ps1)
+
+$Deliverable = ApacheLogs1
+$Filter = Write-Host ($Deliverable | Where-Object{ $_.Message -like "10.0.17.22" -and $_.Referrer -like "*index.html"
+                                                    } `
+                                                   | Format-Table | Out-String )
+
+
