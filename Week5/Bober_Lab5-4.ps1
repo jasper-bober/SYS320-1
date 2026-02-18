1c17a4ae649039ef01377c8199a7208749726ee2
jasper-bober <112430968+jasper-bober@users.noreply.github.com>
2026-02-17 22:04:30 -0500

    Add files via upload

---
+++ Week5/Bober_Lab5-4.ps1
@@ -0,0 +1,12 @@
+﻿. (Join-Path $PSScriptRoot Bober_Lab5-3.ps1)
+
+$FullTable2 = daysTranslator($FullTable)
+
+$FullTable2 | Where-Object { $_.Instrcutor -ilike "Furkan*" } 
+# 
+
+
+
+
+
+
---
+++ Week5/Bober_Lab5-6.ps1
@@ -0,0 +1,18 @@
+﻿. (Join-Path $PSScriptRoot Bober_Lab5-3.ps1)
+
+
+$FullTable2 = daysTranslator($FullTable)
+
+$ITSInstructors = $FullTable2 | Where-Object { ($_."Class Code" -ilike "*SYS*") -or `
+                             ($_."Class Code" -ilike "*NET*") -or  `
+                             ($_."Class Code" -ilike "*SEC*") -or `
+                             ($_."Class Code" -ilike "*FOR*") -or `
+                             ($_."Class Code" -ilike "*CSI*") -or `
+                             ($_."Class Code" -ilike "*DAT*") -and
+                             ($_."Instrcutor" -notmatch "\d") }`
+                             | Sort-Object "Instrcutor" | Select-Object -Property "Instrcutor"
+
+#$ITSInstructors
+
+
+ 
