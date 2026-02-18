901938de9af9b53a1bc7a78e354034cebd8dd543
jasper-bober <112430968+jasper-bober@users.noreply.github.com>
2026-02-17 22:07:58 -0500

    Delete Week5 directory

--- Week5/Bober_Lab5-5.ps1
+++
@@ -1,8 +0,0 @@
-﻿. (Join-Path $PSScriptRoot Bober_Lab5-3.ps1)
-
-
-$FullTable2 = daysTranslator($FullTable)
-
-$FullTable2 | Where-Object { ($_.Location -ilike "FREE 105") -and ($_.Days -ilike "Wednesday")} | `
-Sort-Object -Property "Time Start" | Select-Object -Property "Time Start", "Time End", "Class Code"
- 
--- Week5/Bober_Lab5-7.ps1
+++
@@ -1,6 +0,0 @@
-﻿. (Join-Path $PSScriptRoot Bober_Lab5-6.ps1)
-
-
-$FullTable2 | Where-Object { $_.Instructor -in $ITSInstructors.Instructor } `
-| Group-Object "Instructor" | Select-Object -Property Count, Name | Sort-Object Count -Descending
-
