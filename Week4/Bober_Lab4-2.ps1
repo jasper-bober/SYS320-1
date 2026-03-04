82413a1db0ba19f547f958098457e273ffbf59be
jasper-bober <112430968+jasper-bober@users.noreply.github.com>
2026-03-04 02:10:02 -0500

    Update Bober_Lab4-2.ps1

--- Week4/Bober_Lab4-2.ps1
+++ Week4/Bober_Lab4-2.ps1
@@ -1,9 +1,10 @@
-﻿.(Join-Path $PSScriptRoot Bober_LogLab-1.ps1)
+﻿.(Join-Path $PSScriptRoot Bober_Lab4-1.ps1)
 
 $Deliverable = ApacheLogs1
 $Filter = Write-Host ($Deliverable | Where-Object{ $_.Message -like "10.0.17.22" -and $_.Referrer -like "*index.html"
                                                     } `
                                                    | Format-Table | Out-String )
+
 
 
 
